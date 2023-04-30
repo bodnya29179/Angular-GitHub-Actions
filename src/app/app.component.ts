82578f11cfc5b612b360fe018e434a118fc413d4
Yevhen Bodnia <bodnya29179@gmail.com>
2023-04-30 16:39:10 +0300

    Add unit tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ``,
+  template: `
+    <button class="increment-btn" (click)="increment()">+1</button>
+
+    <div>{{ counter }}</div>
+
+    <button class="decrement-btn" (click)="decrement()">-1</button>
+  `,
 })
-export class AppComponent {}
+export class AppComponent {
+  counter = 0;
+
+  increment(): void {
+    this.counter++;
+  }
+
+  decrement(): void {
+    this.counter--;
+  }
+}
